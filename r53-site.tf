57331399167fa73b5d214540a28e59edd22052ed
OllieJC <5426038+OllieJC@users.noreply.github.com>
2025-10-24 10:08:34 +0100

    Create r53-site.tf

---
+++ r53-site.tf
@@ -0,0 +1,40 @@
+resource "aws_route53_record" "site-www-cname" {
+  zone_id = aws_route53_zone.ukpsdigital.zone_id
+  name    = "www"
+  type    = "CNAME"
+  ttl     = local.standard_ttl
+
+  records = [
+    "govuk-digital-backbone.github.io."
+  ]
+}
+
+resource "aws_route53_record" "site-www-a" {
+  zone_id = aws_route53_zone.ukpsdigital.zone_id
+  name    = "www"
+  type    = "A"
+  ttl     = local.standard_ttl
+
+  # GitHub pages A records:
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+}
+
+resource "aws_route53_record" "site-www-aaaa" {
+  zone_id = aws_route53_zone.ukpsdigital.zone_id
+  name    = "www"
+  type    = "AAAA"
+  ttl     = local.standard_ttl
+
+  # GitHub pages AAAA records:
+  records = [
+    "2606:50c0:8000::153",
+    "2606:50c0:8001::153",
+    "2606:50c0:8002::153",
+    "2606:50c0:8003::153"
+  ]
+}
