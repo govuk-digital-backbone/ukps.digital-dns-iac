74f244dcea0d5decb408e0bc0698628a5f12035f
OllieJC <5426038+OllieJC@users.noreply.github.com>
2025-12-02 22:51:50 +0000

    Remove security.txt Route 53 record
    Removed the aws_route53_record resource for security.txt.

--- r53.tf
+++ r53.tf
@@ -45,18 +45,6 @@ resource "aws_route53_record" "security_txt-prod" {
   ]
 }
 
-resource "aws_route53_record" "security_txt-prod" {
-  zone_id = aws_route53_zone.ukpsdigital.zone_id
-  name    = "_security"
-  type    = "TXT"
-  ttl     = local.standard_ttl
-
-  records = [
-    "security_policy=https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt",
-    "security_contact=https://vulnerability-reporting.service.security.gov.uk/submit",
-  ]
-}
-
 module "aws-r53-parked-domain" {
   source            = "github.com/co-cddo/aws-route53-parked-govuk-domain//terraform?ref=5e85556ce417cd335c440fd1e7079bd331f443d5"
   zone_id           = aws_route53_zone.ukpsdigital.zone_id
